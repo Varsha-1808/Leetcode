e8f16a0fa41a5ab2f33305df4c43f685dd62742d
Varsha Thangaraj <varshathangaraj1808@gmail.com>
2026-09-13 22:29:20 +0530

    Time: 1 ms (98.07%), Space: 42.4 MB (90.67%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int addDigits(int num) {
+        if(num>=0 && num<=9){
+            return num;
+        }
+        int sum=0;
+        while(num>9){
+          sum=0;
+        while(num!=0){
+            int rem=num%10;
+            sum+=rem;
+            num/=10;
+        }
+        num=sum;
+        }
+        return sum;
+    }
+}
